258c3ed5a7c5d22f2905bcb996e9b9dbac57fd4b
ishalandge2609 <120173940+ishalandge2609@users.noreply.github.com>
2026-03-24 22:09:43 +0530

    added week 6 day 25

---
+++ week6/day25/problem3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+
+        static void GenerateSalesReport()
+        {
+            Console.WriteLine("Sales Report started...");
+            Thread.Sleep(3000);
+            Console.WriteLine("Sales Report completed.");
+        }
+
+
+        static void GenerateInventoryReport()
+        {
+            Console.WriteLine("Inventory Report started...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Inventory Report completed.");
+        }
+
+
+        static void GenerateCustomerReport()
+        {
+            Console.WriteLine("Customer Report started...");
+            Thread.Sleep(2500);
+            Console.WriteLine("Customer Report completed.");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Report generation started...\n");
+
+
+            Task task1 = Task.Run(() => GenerateSalesReport());
+            Task task2 = Task.Run(() => GenerateInventoryReport());
+            Task task3 = Task.Run(() => GenerateCustomerReport());
+
+
+            Task.WaitAll(task1, task2, task3);
+
+            Console.WriteLine("\nAll reports generated successfully.");
+
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
