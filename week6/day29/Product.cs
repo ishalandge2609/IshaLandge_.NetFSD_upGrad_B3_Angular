f7b55dc58c4d2a0b9202e8eb0c6450c7eb855632
ishalandge2609 <120173940+ishalandge2609@users.noreply.github.com>
2026-04-01 19:44:30 +0530

    ḍay 29 added

---
+++ week6/day29/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstMVCApp.Models
+{
+    public class Product
+    {
+            public int ProductId { get; set; }
+            public string  ProductName { get; set; }
+            public decimal  ProductPrice { get; set; }
+            public string Category { get; set; }
+        }
+    }
+
